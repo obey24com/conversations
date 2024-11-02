2e0e04fcdcee596d78daee4591e2a99691ceff0c
obey24com <107033246+obey24com@users.noreply.github.com>
2024-11-02 17:28:50 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from "@/components/ui/toaster";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
