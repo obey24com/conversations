75400bd79842640a148bb6f3df6fbb7121cb47d1
obey24com <107033246+obey24com@users.noreply.github.com>
2024-11-02 17:21:02 +0100

    Update layout.tsx
    Google AdSense Code

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7026535539086017"
+     crossorigin="anonymous"></script></head>
       <body className={inter.className}>
         {children}
         <Toaster />
