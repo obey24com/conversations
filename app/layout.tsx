b642949b6feb6a8155ef50aa555785687e1c5271
obey24com <107033246+obey24com@users.noreply.github.com>
2024-11-02 17:26:21 +0100

    Update layout.tsx
    Google Adsense Script

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7026535539086017"
-     crossorigin="anonymous"></script></head>
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7026535539086017"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={inter.className}>
         {children}
         <Toaster />
