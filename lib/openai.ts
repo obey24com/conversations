fbc567b5016a8dbab500e1c0fb9187709322b642
obey24com <107033246+obey24com@users.noreply.github.com>
2024-11-02 18:46:28 +0100

    Update openai.ts
    changing to GPT-4o

--- lib/openai.ts
+++ lib/openai.ts
@@ -17,7 +17,7 @@ export async function translateText(
 ) {
   try {
     const completion = await openai.chat.completions.create({
-      model: 'gpt-4-turbo',
+      model: 'gpt-4o',
       messages: [
         {
           role: 'system',
