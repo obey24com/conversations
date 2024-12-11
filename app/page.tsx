5ba4b91e7eb61a1568056cfdc5360c7c926739be
obey24com <107033246+obey24com@users.noreply.github.com>
2024-12-11 17:17:43 +0100

    Update page.tsx
    removing bg

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,7 @@ import { TranslationInterface } from "@/components/translation-interface";
 
 export default function Home() {
   return (
-    <main
-      className="flex min-h-[calc(100dvh-65px)] flex-col pb-[116px]"
-      style={{
-        background: "linear-gradient(to top, #efefef, #ffffff)",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-      }}
-    >
+    <main className="flex min-h-[calc(100dvh-65px)] flex-col pb-[116px]">
       <TranslationInterface />
     </main>
   );
