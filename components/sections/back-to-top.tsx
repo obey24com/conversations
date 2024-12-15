72ff61563c4ad7177082afe9b1affbf1ccd3cea5
obey24com <107033246+obey24com@users.noreply.github.com>
2024-12-15 06:48:30 +0100

    Update back-to-top.tsx

--- components/sections/back-to-top.tsx
+++ components/sections/back-to-top.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
 import { ArrowUp } from "lucide-react";
 import { cn } from "@/lib/utils";
 
