5c471e986e6a76a028ef80abe32b91d0482532aa
obey24com <107033246+obey24com@users.noreply.github.com>
2024-12-19 05:05:03 +0100

    Update message-bubble.tsx

--- components/message-bubble.tsx
+++ components/message-bubble.tsx
@@ -11,7 +11,7 @@ export interface MessageBubbleProps {
   translation: string;
   fromLang: string;
   toLang: string;
-  cultural?: string;
+  context?: string;
   isPlaying: boolean;
   onPlay: () => void;
   onDelete: () => void;
