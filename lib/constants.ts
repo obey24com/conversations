d61e18b59359a9fd4b6a05bcddcde789d5bf5cde
obey24com <107033246+obey24com@users.noreply.github.com>
2024-12-24 03:50:55 +0100

    Create constants.ts
    Adding Header Download

---
+++ lib/constants.ts
@@ -0,0 +1,11 @@
+export const STORAGE_KEYS = {
+  FROM_LANG: 'ulocat-from-lang',
+  TO_LANG: 'ulocat-to-lang',
+  AUTO_SWITCH: 'ulocat-auto-switch',
+  MESSAGES: 'ulocat-messages',
+} as const;
+
+export const STORE_URLS = {
+  PLAY_STORE: 'https://play.google.com/store/apps/details?id=com.ulocatbyobey24com.ulocat',
+  APP_STORE: '#',
+} as const;
