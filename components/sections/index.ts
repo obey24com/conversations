ffd7c2029ca30db0d12aaf12110edd11f7733f21
obey24com <107033246+obey24com@users.noreply.github.com>
2024-12-27 18:45:49 +0100

    Create index.ts

---
+++ components/sections/index.ts
@@ -0,0 +1,7 @@
+export * from './back-to-top';
+export * from './benefits';
+export * from './comparison';
+export * from './how-it-works';
+export * from './navigation';
+export * from './seo-content';
+export * from './news-teaser';
