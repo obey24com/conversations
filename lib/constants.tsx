a1d446f3fa351cfd88da8282145b315c01e913d6
obey24com <107033246+obey24com@users.noreply.github.com>
2024-12-30 05:18:40 +0100

    Create constants.tsx

---
+++ lib/constants.tsx
@@ -0,0 +1,4 @@
+export const STORE_URLS = {
+  APP_STORE: "https://apps.apple.com/app/ulocat/id123456789", // Coming soon
+  PLAY_STORE: "https://play.google.com/store/apps/details?id=com.ulocatbyobey24com.ulocat",
+} as const;
