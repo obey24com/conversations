0faa23e8405e466def9289e7107ad423e9695415
obey24com <107033246+obey24com@users.noreply.github.com>
2025-01-05 16:21:36 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -88,6 +88,7 @@ const config: Config = {
   plugins: [
     require('tailwindcss-animate'),
     require('@tailwindcss/typography'),
+    require('@tailwindcss/typography'),
   ],
 };
 export default config;
