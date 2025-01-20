4d388eb60f315e40cf3611700fd8e84d35269e5d
obey24com <107033246+obey24com@users.noreply.github.com>
2025-01-20 15:18:10 +0100

    Update and rename middleware.ts to deactivated_middleware.ts

--- deactivated_middleware.ts
+++ deactivated_middleware.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from 'next/server'
+/* 
+import { NextResponse } from 'next/server' 
 import type { NextRequest } from 'next/server'
-
 const DESKTOP_DOMAIN = 'https://desktop.ulocat.com'
 
 function isMobileDevice(userAgent: string): boolean {
