2c5a01c418fd4bc68ce57a6741e3b5413799fd33
obey24com <107033246+obey24com@users.noreply.github.com>
2025-03-21 10:55:48 +0100

    Update page.tsx
    background change

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { TranslationInterface } from "@/components/translation-interface";
 
 export default function Home() {
   return (
-    <main className="flex min-h-[calc(100dvh-65px)] flex-col pb-[116px] animated-dots bg-white/50">
+    <main className="flex min-h-[calc(100dvh-65px)] flex-col pb-[116px] animated-dots">
       <TranslationInterface />
     </main>
   );
