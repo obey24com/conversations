a057fd01eedfdbd4daad51b30a1daa2a48956678
obey24com <107033246+obey24com@users.noreply.github.com>
2025-03-21 15:02:53 +0100

    Update toaster.tsx

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -14,10 +14,10 @@ export function Toaster() {
   const { toasts } = useToast();
 
   return (
-    <ToastProvider>
+    <ToastProvider swipeDirection="up">
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          <Toast key={id} {...props}>
+          <Toast key={id} {...props} className="z-[300]">
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
