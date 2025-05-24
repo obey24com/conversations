fa2c104c39cc4e90f9ec99a10b53b8e9a180bf8e
obey24com <107033246+obey24com@users.noreply.github.com>
2025-05-24 12:46:26 +0700

    Update openai.ts

--- lib/openai.ts
+++ lib/openai.ts
@@ -171,7 +171,8 @@ export async function textToSpeech(text: string) {
       model: 'tts-1-hd',
       voice: 'shimmer',
       input: text,
-      speed: 1.0,
+      speed: 0.7,
+      instructions: "Voice Affect: Slow speaking, Clear toning, no accent;.\n\nTone: In a way to make someone very clear what you are talking about. \n\nEmotion: Positive energy. \n\nPersonality: Relatable and engaging. \n\nPauses: Normal, like in a daily conversation use purposeful. Pauses after key moments.",
       response_format: "mp3"
     });
 
